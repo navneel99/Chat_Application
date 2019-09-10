63ba40b284d0983de4749ba8226eecd4f9910b66
Navneel Mandal <navneel99@gmail.com>
2019-09-10 23:05:57 +0530

    Registering Basic Done.

--- client.py
+++ client.py
@@ -1,11 +1,31 @@
 import socket
 # import re
 
-def register_user(u_name):
-#    if (check_username(u_name)):
-    payload = "REGISTER TOSEND ", u_name,"\n\n"
-    payload2 = "REGISTER TORECV ",u_name,"\n\n"
+def encrypt_decrypt(data,flag=True): #True flag means encryption
+    if flag:
+        return bytes(data,encoding="ascii")
+    else:
+        return (data.decode(encoding = "ascii"))
 
+def register_user(u_name, send_socket, receive_socket):
+    payload = "REGISTER TOSEND "+u_name+"\n\n"
+    payload2 = "REGISTER TORECV "+u_name+"\n\n"
+    for curr_payload in [payload,payload2]:
+        send_socket.send(encrypt_decrypt(curr_payload))
+        ack = receive_socket.recv(1024)
+        # print (ack)
+        # print(encrypt_decrypt(ack,False))
+        ack_string = [line.split() for line in (encrypt_decrypt(ack,False)).split('\n')]
+        # print (ack_string)
+        if ack_string[0][0] == "REGISTERED":
+            continue
+        else:
+            # break
+            print ("Malformed Username")
+            return False
+    else:
+        print ("User Registered.")
+        return True
 
 s1 = socket.socket()
 s2 = socket.socket()
@@ -14,17 +34,24 @@ s_port = 6970 #sending port of the server
 s1.connect(('127.0.0.1',r_port)) #It'll send data for the client
 s2.connect(('127.0.0.1',s_port)) #It'll receive data for the client
 print ("You are successfully connected to the server\n")
-get_out = False
-u_name = input("Please Enter your username: ")
+
+username_retry = True
 get_out=True
+
+while username_retry:
+    u_name = input("Please Enter your username: ")
+    if register_user(u_name, s1, s2):
+        username_retry = False
+    else:
+        continue
 
 while get_out:
     inp = input("Enter data to be sent to the server. Type q to exit\n")
     if (inp.lower() == 'q'):
         get_out = False
     else:
-        s1.send(bytes(inp, encoding="ascii"))
-        data = s2.recv(1024)
+        s1.send(encrypt_decrypt(inp))
+        data = encrypt_decrypt(s2.recv(1024),False)
         if not data:
             continue
         else:
--- server.py
+++ server.py
@@ -5,31 +5,63 @@ import re
 from socketserver import ThreadingMixIn
 
 def reg_match(regex, string):
-    return (False if len(re.findall(regex,string)) ==0 else True)
+    if re.search(regex, string):
+        return True
+    else:
+        return False
+
+def encrypt_decrypt(data,flag=True): #True flag means encryption
+    if flag:
+        return bytes(data,encoding="ascii")
+    else:
+        return (data.decode(encoding = "ascii"))
+
 # client_lock = threading.Lock()
 # The function sends data to the client and then receives data from the client to return to its main function
-
+users = []
 class ClientPerThread(threading.Thread):
     def __init__(self,r_sock,s_sock):
         threading.Thread.__init__(self)
         self.receiveSocket = r_sock
         self.sendSocket = s_sock
 
+    def client_operations(self):
+        while True:
+            data = encrypt_decrypt(self.receiveSocket.recv(1024),False)
+            split_data = [ line.split() for line in (data.split("\n"))]
+            # print (split_data)
+            if split_data[0][0] == "REGISTER":
+                username = split_data[0][2]
+                if reg_match("[a-zA-Z0-9]+", username):
+                    # print("regex correct")
+                    users.append((username))
+                    print ("REGISTERED "+ split_data[0][1]+" "+username+"\n \n\n")
+                    self.sendSocket.send(encrypt_decrypt("REGISTERED "+ split_data[0][1]+" "+username+"\n \n\n"))
+                else:
+                    self.sendSocket.send(encrypt_decrypt("ERROR 100 Malformed username\n \n\n"))
+            else:
+                print("Normal data from "+username+": "+ str(data))
+                self.sendSocket.send(encrypt_decrypt("Dummy to allow next loop.\n"))
+                # pass
+
+
     def run(self):
        while True:
-            send_data = b"True"
-            if send_data:
-                self.sendSocket.send(send_data) #Need to preprocess this data before sending.
+            # send_data = b"True"
+            # if send_data:
+                # self.sendSocket.send(send_data) #Need to preprocess this data before sending.
             print ("Server waiting for data from client: ")
-            inp_data = self.receiveSocket.recv(1024)
-            if not inp_data:
-                print("Breaking: ")
+            # inp_data = self.receiveSocket.recv(1024)
+            self.client_operations()
+            # if not inp_data:
+                # print("Breaking: ")
                 # client_lock.release()
-                break
-            else:
-                print ("Data from client: ", inp_data)
+                # break
+            # else:
+                # print ("Data from client: ", inp_data)
        print ("Exited form the thread.\n")
        # return inp_data
+
 
 
 def main_server_body():
@@ -60,8 +92,8 @@ def main_server_body():
         threads.append(newthread)
         print ("Accepted  receive connection from: "+ str(r_address))
         print ("Acceoted send connection from: " + str(s_address))
-        s_conn.send(b"You have connected to the server")
-        data = b"DUMMY"
+
+        # data = b"DUMMY"
         # start_new_thread(thread_client, (conn,conn2,data))
 
 main_server_body()
